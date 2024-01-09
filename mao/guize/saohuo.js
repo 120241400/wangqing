77f6821de664509741b8d927bb71fea414fe7a55
120241400 <89237995+120241400@users.noreply.github.com>
2024-01-09 20:26:57 +0800

    Update saohuo.js

--- mao/guize/saohuo.js
+++ mao/guize/saohuo.js
@@ -2,7 +2,7 @@
 var rule={
     title:'骚火电影',
     // host:'https://saohuo.us',
-    host:'https://shdy3.com?http://shapp.us',
+    host:'https://shaohuo.vip',
     hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"a:eq(2)&&href");print(src);HOST=src',
     // url:'/list/fyclass-fypage.html',
     url:'/list/fyfilter-fypage.html',
